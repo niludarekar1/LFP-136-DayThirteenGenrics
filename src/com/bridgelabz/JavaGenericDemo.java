9ea58b6e7e4be7d3cfa419c61c27ce32fe89af78
niludarekar1 <nilu.darekar1@gmail.com>
2022-05-15 22:21:48 +0530

    Modified: Max 3 Float Number

---
+++ src/com/bridgelabz/JavaGenericDemo.java
@@ -0,0 +1,45 @@
+package com.bridgelabz;
+
+public class JavaGenericDemo {
+    //compareTo method for Checking Maximum from 3 Integer Object
+    public static Integer compareTo(Integer a,Integer b,Integer c) {
+        if(a > b){
+            if(a > c) {
+                return a;
+            } else {
+                return c;
+            }
+        } else {
+            if(b > c) {
+                return b;
+            } else {
+                return c;
+            }
+        }
+    }
+
+    //compareTo method for Checking Maximum from 3 Float Object
+    public static Float compareTo(Float a,Float b,Float c) {
+        if(a > b){
+            if(a > c) {
+                return a;
+            } else {
+                return c;
+            }
+        } else {
+            if(b > c) {
+                return b;
+            } else {
+                return c;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        //Welcome message for User
+        System.out.println("Welcome to Java Core - Generics Test Maximum .");
+
+        System.out.println("Maximum from 3 Integer Object : "+compareTo(90,120,190));
+        System.out.println("Maximum from 3 Float Object : "+compareTo(90.0F,120.0F,190.0F));
+    }
+}
